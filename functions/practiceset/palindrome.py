4eaee19d331987ee15bb067267944c0ea1f7ab96
jam92444 <jamalyusuf579@gmail.com>
2025-02-21 22:11:20 +0530

    added flow of code

--- functions/practiceset/palindrome.py
+++ functions/practiceset/palindrome.py
@@ -4,13 +4,13 @@ def reverse_string(word):#word=pop
 def palindrome(word):#word=pop
     string = word; #string = "pop"
     reversed_string = reverse_string(string)  # Correctly call the function to reverse the word
-#reversed_string = pop
+    #reversed_string = pop
     #if pop == pop:
     if string == reversed_string:  # Compare the word and its reversed version
         return f"{word} is a palindrome"
     else:
         return f"{word} is not a palindrome"
 
-word = input("Enter the word: ")#pop
-isPalindrome = palindrome(word); #ispalindrome = "pop is a palindrome"
+word = input("Enter the word: ")#pop #1 run 
+isPalindrome = palindrome(word); #ispalindrome = "pop is a palindrome" #2 run function all palindrome parameter word
 print(isPalindrome)
